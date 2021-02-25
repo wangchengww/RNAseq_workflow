0a6516167b75652e95b487061e6dc9d137f9c63a
laowang1992 <763548707@qq.com>
2021-02-25 19:27:25 +0800

    some R script

--- 03.Merge_result/stat_plot.R
+++ 03.Merge_result/stat_plot.R
@@ -1,10 +1,10 @@
-# 加载扩展�?
+# 加载扩展???
 library(tidyverse)
 library(ggplot2)
-library(cowplot)
 library(ggsci)
+library(cowplot)
 library(reshape2)
-# 读取数据并转换格�?
+# 读取数据并转换格???
 df <- read.table(file="genes.TMM.EXPR.matrix", header=T)
 head(df)
 d <- melt(df)
@@ -25,8 +25,8 @@ p1 <- ggplot(data=d, aes(x=variable, y=log10(value+1), fill=variable)) +
   labs(fill="Sample") +
   #scale_fill_aaas() +
   mytheme
-ggsave(p1, filename="genes.TMM.EXPR.pdf", height = 5, width = 10)
-ggsave(p1, filename="genes.TMM.EXPR.png", height = 5, width = 10, dpi = 500)
+ggsave(p1, filename="genes.TMM.EXPR.pdf", height = 5, width = 13)
+ggsave(p1, filename="genes.TMM.EXPR.png", height = 5, width = 13, dpi = 500)
 
 p2 <-ggplot(data=d, aes(x=log10(value+1), color=variable)) +
   geom_density(size=1) +
@@ -34,8 +34,8 @@ p2 <-ggplot(data=d, aes(x=log10(value+1), color=variable)) +
   ylab("Density")+
   labs(fill="Sample") +
   mytheme
-ggsave(p2, filename="genes.TMM.EXPR.density.pdf", height = 4, width = 6)
-ggsave(p2, filename="genes.TMM.EXPR.density.png", height = 4, width = 6, dpi = 500)
+ggsave(p2, filename="genes.TMM.EXPR.density.pdf", height = 4, width = 8.5)
+ggsave(p2, filename="genes.TMM.EXPR.density.png", height = 4, width = 8.5, dpi = 500)
 
 
 gene_exp <- read.table("genes.TMM.EXPR.matrix", 
@@ -46,93 +46,95 @@ head(metadata)
 
 # PCA
 library(PCAtools)
-p <- pca(gene_exp, metadata = metadata)
+p <- pca(log10(gene_exp+1), metadata = metadata)
+pdf(file = "screeplot.pdf", width = 10, height = 5)
 screeplot(p)
-biplot(p, 
-       x = 'PC1',                 # x �?
-       y = 'PC2',                 # y �?
-       colby = 'Group',          # 颜色映射
-       shape = 'Genotype',
-       legendPosition = 'right',  # 图例位置
-       lab = rownames(metadata)                    # 样本名称显示
-)
 dev.off()
+png(file = "screeplot.png", width = 10, height = 5, units = "in", res = 500)
+screeplot(p)
+dev.off()
+#biplot(p, 
+#       x = 'PC1',                 # x ???
+#       y = 'PC2',                 # y ???
+#       colby = 'Strain',          # 颜色映射
+#       shape = 'Stage',
+#       legendPosition = 'right',  # 图例位置
+#       lab = rownames(metadata)                    # 样本名称显示
+#)
+
 pca_rotated_plus <- rownames_to_column(p$rotated, 
                                        var = 'sample_name') %>%
   left_join(rownames_to_column(metadata, var = 'sample_name'), 
             by = 'sample_name')
-write.table(x = pca_rotated_plus, file = "PCA.txt", sep = "\t", row.names = F, quote = F)
-write.csv(x = pca_rotated_plus, file = "PCA.csv", row.names = F, quote = F)
+
 
 p_pca <- ggplot(data = pca_rotated_plus, aes(x = PC1, y = PC2)) +
   geom_point(size = 6, 
-             aes(shape = Genotype, fill = Group)) +
-  stat_ellipse(aes(color = Tissue)) +
-  scale_shape_manual(values = c(21:23)) +
-  scale_fill_npg() +
-  scale_color_aaas() +
-  labs(x = 'PC1 (74.68% variance explained)',
-       y = 'PC2 (10.98% variance explained') +
+             aes(shape = Strain, fill = Stage)) +
+  #stat_ellipse(aes(color = Stage)) +
+  scale_shape_manual(values = c(21, 22, 23, 24)) +
+  scale_fill_aaas() +
+  scale_color_npg() +
+  labs(x = paste("PC1 (", round(p[["variance"]][["PC1"]], 2), "% variance explained)", sep = ""),
+       y = paste("PC1 (", round(p[["variance"]][["PC2"]], 2), "% variance explained)", sep = "")) +
   theme_half_open() + 
   theme(#legend.position = c(0.2, 0.9), 
     legend.background = element_rect(fill = NA)
-    #legend.direction = "horizontal"
   ) +
   guides(fill = guide_legend(override.aes=list(shape=21)))
-p_pca
-ggsave(p_pca, filename = "PCAplot_PC1&PC2.pdf", height = 5, width = 7)
-ggsave(p_pca, filename = "PCAplot_PC1&PC2.png", height = 5, width = 7, dpi = 500)
 
+ggsave("PCAplot_PC1&PC2.pdf", height = 5, width = 7)
+ggsave("PCAplot_PC1&PC2.png", height = 5, width = 7, dpi = 500)
 
-# 相关�?
-sample_cor <- cor(log10(gene_exp + 1), 
-                  method = "pearson") # 算法�? pearson | kendall | spearman
+# 相关???
+sample_cor <- cor(log10(gene_exp+1), 
+                  method = "pearson") # 算法??? pearson | kendall | spearman
 write.table(x = as.table(sample_cor), file = "./correlation.txt")
 write.csv(x = as.table(sample_cor), file = "./correlation.csv")
 
 #annotation_colors = list( treatment = c(ck = "red", PW = "orange"))
 
-pdf("correlation_withClust.pdf",width = 9,height = 7)
-pheatmap::pheatmap(sample_cor,                # 绘图数据，应进行log转换，log2或log10，并且防止表达量�?0应该df+1
+pdf("correlation_withClust.pdf",width = 12,height = 11)
+pheatmap::pheatmap(sample_cor,                # 绘图数据，应进行log转换，log2或log10，并且防止表达量???0应该df+1
                    show_rownames = TRUE,      # 是否显示行名（基因名），基因太多时选FALSE
                    show_colnames = TRUE,      # 是否显示列明（样本名），同上
                    cluster_rows = T,       # 是否对行（基因）聚类
                    cluster_cols = T,       # 是否对列（样本）聚类
-                   border_color = "NA",       # 网格分割线颜�?
-                   #cutree_col = 3,            # 热图按列分为3�?
-                   #cutree_rows = 2,           # 热图按行分为2�?
+                   border_color = "NA",       # 网格分割线颜???
+                   #cutree_col = 3,            # 热图按列分为3???
+                   #cutree_rows = 2,           # 热图按行分为2???
                    display_numbers = FALSE,   # 是否显示数值，数值为上面转换过的大小
-                   number_format = "%.2f",    # 数值保留小数点�?2位数，当display_numbers=TRUE有效
+                   number_format = "%.2f",    # 数值保留小数点???2位数，当display_numbers=TRUE有效
                    #annotation_colors = annotation_colors,
-                   scale = "none",            # �?"row", "column"�?"none"，是否要进行标准化，如果要突出所有基因在处理组和对照组之间表达有差异，建议改�?"row"，如果要突出在所有样本中基因的分组情况应改为"column"
-                   annotation_row = data.frame(Tissue=metadata$Tissue, row.names = row.names(metadata)), # 添加列注释，col_metadata.csv中信�?
-                   annotation_col = data.frame(Group=metadata$Group,Genotype=metadata$Genotype,row.names = rownames(metadata)), # 添加行注释，row_metadata.csv中信�?
+                   scale = "none",            # ???"row", "column"???"none"，是否要进行标准化，如果要突出所有基因在处理组和对照组之间表达有差异，建议改???"row"，如果要突出在所有样本中基因的分组情况应改为"column"
+                   annotation_col = data.frame(strain=metadata$Strain,row.names = rownames(metadata)), # 添加列注释，col_metadata.csv中信???
+                   annotation_row = data.frame(stage=metadata$Stage,row.names = rownames(metadata)), # 添加行注释，row_metadata.csv中信???
                    cellwidth = 12,
                    cellheight = 12,
-                   color = colorRampPalette(c("blue","white","red"))(100),
+                   color = colorRampPalette(c("green","white","red"))(100),
                    #annotation_legend = F,
                    #legend = F
                    #main = "example heatmap"   # 添加标题
 )
 dev.off()
-png("correlation_withClust.png", width = 9, height = 7, units = "in", res = 500)
-pheatmap::pheatmap(sample_cor,                # 绘图数据，应进行log转换，log2或log10，并且防止表达量�?0应该df+1
+png("correlation_withClust.png", width = 12, height = 11, units = "in", res = 500)
+pheatmap::pheatmap(sample_cor,                # 绘图数据，应进行log转换，log2或log10，并且防止表达量???0应该df+1
                    show_rownames = TRUE,      # 是否显示行名（基因名），基因太多时选FALSE
                    show_colnames = TRUE,      # 是否显示列明（样本名），同上
                    cluster_rows = T,       # 是否对行（基因）聚类
                    cluster_cols = T,       # 是否对列（样本）聚类
-                   border_color = "NA",       # 网格分割线颜�?
-                   #cutree_col = 3,            # 热图按列分为3�?
-                   #cutree_rows = 2,           # 热图按行分为2�?
+                   border_color = "NA",       # 网格分割线颜???
+                   #cutree_col = 3,            # 热图按列分为3???
+                   #cutree_rows = 2,           # 热图按行分为2???
                    display_numbers = FALSE,   # 是否显示数值，数值为上面转换过的大小
-                   number_format = "%.2f",    # 数值保留小数点�?2位数，当display_numbers=TRUE有效
+                   number_format = "%.2f",    # 数值保留小数点???2位数，当display_numbers=TRUE有效
                    #annotation_colors = annotation_colors,
-                   scale = "none",            # �?"row", "column"�?"none"，是否要进行标准化，如果要突出所有基因在处理组和对照组之间表达有差异，建议改�?"row"，如果要突出在所有样本中基因的分组情况应改为"column"
-                   annotation_row = data.frame(Tissue=metadata$Tissue, row.names = row.names(metadata)), # 添加列注释，col_metadata.csv中信�?
-                   annotation_col = data.frame(Group=metadata$Group,Genotype=metadata$Genotype,row.names = rownames(metadata)), # 添加行注释，row_metadata.csv中信�?
+                   scale = "none",            # ???"row", "column"???"none"，是否要进行标准化，如果要突出所有基因在处理组和对照组之间表达有差异，建议改???"row"，如果要突出在所有样本中基因的分组情况应改为"column"
+                   annotation_col = data.frame(strain=metadata$Strain,row.names = rownames(metadata)), # 添加列注释，col_metadata.csv中信???
+                   annotation_row = data.frame(stage=metadata$Stage,row.names = rownames(metadata)), # 添加行注释，row_metadata.csv中信???
                    cellwidth = 12,
                    cellheight = 12,
-                   color = colorRampPalette(c("blue","white","red"))(100),
+                   color = colorRampPalette(c("green","white","red"))(100),
                    #annotation_legend = F,
                    #legend = F
                    #main = "example heatmap"   # 添加标题
@@ -140,47 +142,47 @@ pheatmap::pheatmap(sample_cor,                # 绘图数据，应进行log转�
 dev.off()
 
 
-pdf("correlation_withoutClust.pdf",width = 9,height = 7)
-pheatmap::pheatmap(sample_cor,                # 绘图数据，应进行log转换，log2或log10，并且防止表达量�?0应该df+1
+pdf("correlation_withoutClust.pdf",width = 12,height = 11)
+pheatmap::pheatmap(sample_cor,                # 绘图数据，应进行log转换，log2或log10，并且防止表达量???0应该df+1
                    show_rownames = TRUE,      # 是否显示行名（基因名），基因太多时选FALSE
                    show_colnames = TRUE,      # 是否显示列明（样本名），同上
                    cluster_rows = F,       # 是否对行（基因）聚类
                    cluster_cols = F,       # 是否对列（样本）聚类
-                   border_color = "NA",       # 网格分割线颜�?
-                   #cutree_col = 3,            # 热图按列分为3�?
-                   #cutree_rows = 2,           # 热图按行分为2�?
+                   border_color = "NA",       # 网格分割线颜???
+                   #cutree_col = 3,            # 热图按列分为3???
+                   #cutree_rows = 2,           # 热图按行分为2???
                    display_numbers = FALSE,   # 是否显示数值，数值为上面转换过的大小
-                   number_format = "%.2f",    # 数值保留小数点�?2位数，当display_numbers=TRUE有效
+                   number_format = "%.2f",    # 数值保留小数点???2位数，当display_numbers=TRUE有效
                    #annotation_colors = annotation_colors,
-                   scale = "none",            # �?"row", "column"�?"none"，是否要进行标准化，如果要突出所有基因在处理组和对照组之间表达有差异，建议改�?"row"，如果要突出在所有样本中基因的分组情况应改为"column"
-                   annotation_row = data.frame(Tissue=metadata$Tissue, row.names = row.names(metadata)), # 添加列注释，col_metadata.csv中信�?
-                   annotation_col = data.frame(Group=metadata$Group,Genotype=metadata$Genotype,row.names = rownames(metadata)), # 添加行注释，row_metadata.csv中信�?
+                   scale = "none",            # ???"row", "column"???"none"，是否要进行标准化，如果要突出所有基因在处理组和对照组之间表达有差异，建议改???"row"，如果要突出在所有样本中基因的分组情况应改为"column"
+                   annotation_col = data.frame(strain=metadata$Strain,row.names = rownames(metadata)), # 添加列注释，col_metadata.csv中信???
+                   annotation_row = data.frame(stage=metadata$Stage,row.names = rownames(metadata)), # 添加行注释，row_metadata.csv中信???
                    cellwidth = 12,
                    cellheight = 12,
-                   color = colorRampPalette(c("blue","white","red"))(100),
+                   color = colorRampPalette(c("green","white","red"))(100),
                    #annotation_legend = F,
                    #legend = F
                    #main = "example heatmap"   # 添加标题
 )
 dev.off()
-png("correlation_withoutClust.png", width = 9, height = 7, units = "in", res = 500)
-pheatmap::pheatmap(sample_cor,                # 绘图数据，应进行log转换，log2或log10，并且防止表达量�?0应该df+1
+png("correlation_withoutClust.png", width = 12, height = 11, units = "in", res = 500)
+pheatmap::pheatmap(sample_cor,                # 绘图数据，应进行log转换，log2或log10，并且防止表达量???0应该df+1
                    show_rownames = TRUE,      # 是否显示行名（基因名），基因太多时选FALSE
                    show_colnames = TRUE,      # 是否显示列明（样本名），同上
                    cluster_rows = F,       # 是否对行（基因）聚类
                    cluster_cols = F,       # 是否对列（样本）聚类
-                   border_color = "NA",       # 网格分割线颜�?
-                   #cutree_col = 3,            # 热图按列分为3�?
-                   #cutree_rows = 2,           # 热图按行分为2�?
+                   border_color = "NA",       # 网格分割线颜???
+                   #cutree_col = 3,            # 热图按列分为3???
+                   #cutree_rows = 2,           # 热图按行分为2???
                    display_numbers = FALSE,   # 是否显示数值，数值为上面转换过的大小
-                   number_format = "%.2f",    # 数值保留小数点�?2位数，当display_numbers=TRUE有效
+                   number_format = "%.2f",    # 数值保留小数点???2位数，当display_numbers=TRUE有效
                    #annotation_colors = annotation_colors,
-                   scale = "none",            # �?"row", "column"�?"none"，是否要进行标准化，如果要突出所有基因在处理组和对照组之间表达有差异，建议改�?"row"，如果要突出在所有样本中基因的分组情况应改为"column"
-                   annotation_row = data.frame(Tissue=metadata$Tissue, row.names = row.names(metadata)), # 添加列注释，col_metadata.csv中信�?
-                   annotation_col = data.frame(Group=metadata$Group,Genotype=metadata$Genotype,row.names = rownames(metadata)), # 添加行注释，row_metadata.csv中信�?
+                   scale = "none",            # ???"row", "column"???"none"，是否要进行标准化，如果要突出所有基因在处理组和对照组之间表达有差异，建议改???"row"，如果要突出在所有样本中基因的分组情况应改为"column"
+                   annotation_col = data.frame(strain=metadata$Strain,row.names = rownames(metadata)), # 添加列注释，col_metadata.csv中信???
+                   annotation_row = data.frame(stage=metadata$Stage,row.names = rownames(metadata)), # 添加行注释，row_metadata.csv中信???
                    cellwidth = 12,
                    cellheight = 12,
-                   color = colorRampPalette(c("blue","white","red"))(100),
+                   color = colorRampPalette(c("green","white","red"))(100),
                    #annotation_legend = F,
                    #legend = F
                    #main = "example heatmap"   # 添加标题
---
+++ 04.DE_analysis/Volcano_plot.R
@@ -0,0 +1,81 @@
+library(argparser, quietly=TRUE)
+
+p <- arg_parser("create volcano plot")
+
+# Add command line arguments
+p <- add_argument(p, "--de_result", help = "DE_result obtained form DEseq2", type = "character")
+p <- add_argument(p, "--select_genes", help = "a file contain GeneID to label on the plot, one gene one row", type = "character")
+p <- add_argument(p, "--padj_cutoff", help = "significance level", type = "numeric", default = 0.05)
+p <- add_argument(p, "--log2FC_cutoff", help = "if absolute value of log2FC is larger than log2FC_cutoff, It's a DEG, default: 1", type = "numeric", default = 1)
+p <- add_argument(p, "--height", help="volcano plot height, units: 'in'", type="numeric", default = 5)
+p <- add_argument(p, "--width", help="volcano plot width, units: 'in'", type="numeric", default = 6)
+
+# Parse the command line arguments
+argv <- parse_args(p)
+
+filename <- argv$de_result
+select_geneID <- argv$select_genes
+padj_cutoff <- argv$padj_cutoff
+log2FC_cutoff <- argv$log2FC_cutoff
+width <- argv$width
+height <- argv$height
+
+#filename <- "./genes.counts.matrix.XiaoYun_4d_vs_Darmor_4d.DESeq2.DE_results"
+#select_geneID <- "./select.txt"
+#padj_cutoff <- 0.05
+#log2FC_cutoff <- 1
+#width <- 6
+#height <- 5
+
+library(tidyverse)
+library(ggsci)
+library(cowplot)
+
+de_result <- read.table(file = filename) %>% rownames_to_column("GeneID") %>%
+  select(GeneID, baseMean, log2FoldChange, padj) %>%
+  mutate(direction = if_else(padj > padj_cutoff | abs(log2FoldChange) < log2FC_cutoff, "NS",
+                             if_else(log2FoldChange >= log2FC_cutoff, "UP", "DOWN")))
+stat <- de_result %>% group_by(direction) %>% count()
+up <- stat %>% filter(direction == "UP") %>% pull(n)
+down <- stat %>% filter(direction == "DOWN") %>% pull(n)
+
+if (is.na(select_geneID)) {
+  Pvolcano <- ggplot(data = de_result, aes(x = log2FoldChange, y = -log10(padj))) +
+    geom_point(size = 2, aes(color = direction), show.legend = F) +
+    #geom_text(data = res_selected, aes(label = GENE_NAME)) +
+    #geom_point(data = res_selected, size = 3, shape = 21, stroke = 1) +
+    #geom_text_repel(data = res_selected, aes(label = GENE_NAME)) +
+    geom_hline(yintercept = -log10(padj_cutoff), 
+               linetype = 'dotdash', color = 'grey30') +
+    geom_vline(xintercept = c(-log2FC_cutoff, log2FC_cutoff), 
+               linetype = 'dotdash', color = 'grey30') +
+    annotate("text", x = max(de_result$log2FoldChange), y = max(-log10(de_result$padj)), 
+             label = paste("Up ", up, "\n", "Down ", down, sep = ""), 
+             vjust=1, hjust=1, colour="black", size=4) +
+    scale_color_manual(values = c('#1613BF', '#727272', '#D5161A')) +
+    theme_half_open()
+    Pvolcano
+} else {
+  library(ggrepel)
+  selected_genes <- read_tsv(file = select_geneID, col_names = F) %>% pull(X1)
+  res_selected <- filter(de_result, GeneID %in% selected_genes)
+  Pvolcano <- ggplot(data = de_result, aes(x = log2FoldChange, y = -log10(padj))) +
+    geom_point(size = 2, aes(color = direction), show.legend = F) +
+    #geom_text(data = res_selected, aes(label = GeneID)) +
+    geom_point(data = res_selected, size = 2, shape = 21, stroke = 1.5) +
+    geom_text_repel(data = res_selected, aes(label = GeneID)) +
+    geom_hline(yintercept = -log10(padj_cutoff), 
+               linetype = 'dotdash', color = 'grey30') +
+    geom_vline(xintercept = c(-log2FC_cutoff, log2FC_cutoff), 
+               linetype = 'dotdash', color = 'grey30') +
+    annotate("text", x = max(de_result$log2FoldChange), y = max(-log10(de_result$padj)), 
+             label = paste("Up ", up, "\n", "Down ", down, sep = ""), 
+             vjust=1, hjust=1, colour="black", size=4) +
+    scale_color_manual(values = c('#1613BF', '#727272', '#D5161A')) +
+    theme_half_open()
+
+}
+
+ggsave(Pvolcano, filename = str_replace(filename, "DE_results", "pdf"), width = width, height = height)
+ggsave(Pvolcano, filename = str_replace(filename, "DE_results", "png"), width = width, height = height, dpi = 500)
+
